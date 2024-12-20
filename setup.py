6a0f9a887fc20058fae138b69e3a4ad921b92479
Amish929 <agarwalamish624@gmail.com>
2024-12-20 16:45:47 +0530

    Files code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Amish929",
+    author_email= "agarwalamish624@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
